3610d89b827b2483b305963568fbe3f4667ca651
dustinii <125335987+dustinii@users.noreply.github.com>
2024-02-06 16:24:53 -0800

    db setup and migration

---
+++ chat_app/migrations/2024-02-07-001917_create_messages/up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
--- chat_app/src/schema.rs
+++ chat_app/src/schema.rs
@@ -1,4 +1,15 @@
-table! {
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    messages (id) {
+        id -> Int4,
+        user_id -> Int4,
+        content -> Text,
+        timestamp -> Timestamp,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
@@ -7,11 +18,9 @@ table! {
     }
 }
 
-table! {
-    messages (id) {
-        id -> Int4,
-        user_id -> Int4,
-        content -> Text,
-        timestamp -> Timestamp,
-    }
-}
+diesel::joinable!(messages -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    messages,
+    users,
+);
